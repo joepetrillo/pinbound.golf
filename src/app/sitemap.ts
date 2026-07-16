4327d16a3e42f67da134561da950336288819317
Joseph Petrillo <jpetrillo119@gmail.com>
2026-07-16 00:16:29 -0400

    cleanups

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,15 +14,11 @@ const staticRoutes = [
 
 const sitemap = (): MetadataRoute.Sitemap => {
   const staticEntries: MetadataRoute.Sitemap = staticRoutes.map((route) => ({
-    changeFrequency: route === "" ? "weekly" : "monthly",
-    priority: route === "" ? 1 : 0.7,
     url: `${SITE_URL}${route}`,
   }));
 
   const blogEntries: MetadataRoute.Sitemap = getBlogPosts().map((post) => ({
-    changeFrequency: "yearly",
     lastModified: post.data.publishedAt,
-    priority: 0.6,
     url: `${SITE_URL}${post.url}`,
   }));
 

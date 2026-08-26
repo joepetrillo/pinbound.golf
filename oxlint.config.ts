a61ecf25e84511267cb416f2ffe0930186bd0b57
Joseph Petrillo <jpetrillo119@gmail.com>
2026-08-26 19:06:26 -0400

    package updates

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "oxlint";
 import antiSlop from "ultracite/oxlint/anti-slop";
 import core from "ultracite/oxlint/core";
-import jsPlugins, { selectJsPlugins } from "ultracite/oxlint/js-plugins";
+import { jsPluginSettings, selectJsPlugins } from "ultracite/oxlint/js-plugins";
 import next from "ultracite/oxlint/next";
 import nextJsPlugins from "ultracite/oxlint/next/js-plugins";
 import react from "ultracite/oxlint/react";
@@ -25,5 +25,5 @@ export default defineConfig({
     // it keeps server-rendered children off the client. See LandingMotion.
     "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
   },
-  settings: jsPlugins.settings,
+  settings: jsPluginSettings,
 });
